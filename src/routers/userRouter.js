ddf576bc7eddeef9a8a35a9c7accdf7f53b5e2b3
Arthur Nepomuceno <arth.nepomuceno@gmail.com>
2022-07-05 02:07:46 -0300

    chore: deploy heroku second attempt

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { validateSignUp } from '../middlewares/validateSignUp.js';
 import { validateSignIn } from '../middlewares/validateSignIn.js';
 import { signUp, signIn} from '../controllers/userControllers.js';
-
+ 
 const userRouter = express.Router();
 userRouter.post('/sign-up', validateSignUp, signUp);
 userRouter.post('/sign-in', validateSignIn, signIn);
