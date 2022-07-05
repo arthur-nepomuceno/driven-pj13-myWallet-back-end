77620add5abd05ea208e79d0314e5f0a21227088
Arthur Nepomuceno <arth.nepomuceno@gmail.com>
2022-07-05 01:17:53 -0300

    feat: first deploy at heroku

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import userRouter from './routers/userRouter.js';
 import transactionsRouter from './routers/transactionRouter.js';
-
+ 
 const server = express();
 server.use(cors());
 server.use(express.json());
